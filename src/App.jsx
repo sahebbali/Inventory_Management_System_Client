55ab1e39e416fdcb69c738773e4583594e15a7cc
sahebbali <sahebbali253@gmail.comm>
2023-06-22 09:41:21 +0600

    add Report Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,23 +12,23 @@ import SendOTPPage from "./pages/Users/SendOTPPage";
 import VerifyOTPPage from "./pages/Users/VerifyOTPPage";
 import CreatePasswordPage from "./pages/Users/CreatePasswordPage";
 import ProfilePage from "./pages/Users/ProfilePage";
-// import BrandCreateUpdatePage from "./pages/Brand/BrandCreateUpdatePage";
-// import BrandListPage from "./pages/Brand/BrandListPage";
-// import CategoryCreateUpdatePage from "./pages/Category/CategoryCreateUpdatePage";
-// import CategoryListPage from "./pages/Category/CategoryListPage";
+import BrandCreateUpdatePage from "./pages/Brand/BrandCreateUpdatePage";
+import BrandListPage from "./pages/Brand/BrandListPage";
+import CategoryCreateUpdatePage from "./pages/Category/CategoryCreateUpdatePage";
+import CategoryListPage from "./pages/Category/CategoryListPage";
 import CustomerCreateUpdatePage from "./pages/Customer/CustomerCreateUpdatePage";
 import CustomerListPage from "./pages/Customer/CustomerListPage";
 import ExpenseTypeCreateUpdatePage from "./pages/ExpenseType/ExpenseTypeCreateUpdatePage";
 import ExpenseListPage from "./pages/Expense/ExpenseListPage";
 import ExpenseCreateUpdatePage from "./pages/Expense/ExpenseCreateUpdatePage";
-// import ProductCreateUpdatePage from "./pages/Product/ProductCreateUpdatePage";
-// import ProductListPage from "./pages/Product/ProductListPage";
+import ProductCreateUpdatePage from "./pages/Product/ProductCreateUpdatePage";
+import ProductListPage from "./pages/Product/ProductListPage";
 // import PurchaseCreateUpdatePage from "./pages/Purchase/PurchaseCreateUpdatePage";
 // import PurchaseListPage from "./pages/Purchase/PurchaseListPage";
-// import PurchaseReportPage from "./pages/Report/PurchaseReportPage";
-// import ReturnReportPage from "./pages/Report/ReturnReportPage";
-// import SaleReportPage from "./pages/Report/SaleReportPage";
-// import ExpenseReportPage from "./pages/Report/ExpenseReportPage";
+import PurchaseReportPage from "./pages/Report/PurchaseReportPage";
+import ReturnReportPage from "./pages/Report/ReturnReportPage";
+import SaleReportPage from "./pages/Report/SaleReportPage";
+import ExpenseReportPage from "./pages/Report/ExpenseReportPage";
 // import ReturnCreateUpdatePage from "./pages/Return/ReturnCreateUpdatePage";
 // import ReturnListPage from "./pages/Return/ReturnListPage";
 // import SalesCreateUpdatePage from "./pages/Sales/SalesCreateUpdatePage";
@@ -43,11 +43,11 @@ const App = () => {
             <Fragment>
                 <BrowserRouter>
                     <Routes>
-                        {/* <Route exact path="/BrandCreateUpdatePage" element={<BrandCreateUpdatePage />}/>
-                        <Route exact path="/BrandListPage" element={<BrandListPage />}/> */}
+                         <Route exact path="/BrandCreateUpdatePage" element={<BrandCreateUpdatePage />}/>
+                        <Route exact path="/BrandListPage" element={<BrandListPage />}/> 
 
-                        {/* <Route exact path="/CategoryCreateUpdatePage" element={<CategoryCreateUpdatePage />}/>
-                        <Route exact path="/CategoryListPage" element={<CategoryListPage />}/> */}
+                        <Route exact path="/CategoryCreateUpdatePage" element={<CategoryCreateUpdatePage />}/>
+                        <Route exact path="/CategoryListPage" element={<CategoryListPage />}/>
 
                         <Route exact path="/CustomerCreateUpdatePage" element={<CustomerCreateUpdatePage />}/>
                         <Route exact path="/CustomerListPage" element={<CustomerListPage />}/>
@@ -58,8 +58,8 @@ const App = () => {
                         <Route exact path="/ExpenseCreateUpdatePage" element={<ExpenseCreateUpdatePage />}/>
                         <Route exact path="/ExpenseListPage" element={<ExpenseListPage />}/>
 
-                        {/* <Route exact path="/ProductCreateUpdatePage" element={<ProductCreateUpdatePage />}/>
-                        <Route exact path="/ProductListPage" element={<ProductListPage />}/> */}
+                        <Route exact path="/ProductCreateUpdatePage" element={<ProductCreateUpdatePage />}/>
+                        <Route exact path="/ProductListPage" element={<ProductListPage />}/> 
 
                         {/* <Route exact path="/PurchaseCreateUpdatePage" element={<PurchaseCreateUpdatePage />}/>
                         <Route exact path="/PurchaseListPage" element={<PurchaseListPage />}/> */}
@@ -72,11 +72,11 @@ const App = () => {
 
                         <Route exact path="/SupplierCreateUpdatePage" element={<SupplierCreateUpdatePage />}/>
                         <Route exact path="/SupplierListPage" element={<SupplierListPage />}/>
-{/* 
+
                         <Route exact path="/PurchaseReportPage" element={<PurchaseReportPage />}/>
                         <Route exact path="/ReturnReportPage" element={<ReturnReportPage />}/>
                         <Route exact path="/SaleReportPage" element={<SaleReportPage />}/>
-                        <Route exact path="/ExpenseReportPage" element={<ExpenseReportPage />}/>  */}
+                        <Route exact path="/ExpenseReportPage" element={<ExpenseReportPage />}/> 
 
                         <Route exact path="/" element={<DashboardPage />}/>
                         <Route exact path="/Profile" element={<ProfilePage/>}/>
